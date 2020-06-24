18252171840e6d3dc9a67da6d305574ba899ffd9
Marcos Dênis <dee.alves2@gmail.com>
2020-06-24 19:56:29 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/denisalves/cursomc/config/Instances.java
+++ src/main/java/com/denisalves/cursomc/config/Instances.java
@@ -7,21 +7,38 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.denisalves.cursomc.domain.Categoria;
+import com.denisalves.cursomc.domain.Produto;
 import com.denisalves.cursomc.repositories.CategoriaRepository;
+import com.denisalves.cursomc.repositories.ProdutoRepository;
 
 @Configuration
 public class Instances implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
