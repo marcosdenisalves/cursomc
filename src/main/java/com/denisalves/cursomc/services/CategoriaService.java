abbf19b640921a896a5d48527ad421d7569826ff
Marcos Dênis <dee.alves2@gmail.com>
2020-06-29 13:06:21 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/denisalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/denisalves/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.denisalves.cursomc.domain.Categoria;
@@ -26,6 +29,11 @@ public class CategoriaService {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id))	;
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 
 	public Categoria insert(Categoria obj) {
