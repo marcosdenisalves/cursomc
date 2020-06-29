99b8f51d8b9d43ec3c968e902b185ea4e3c47da0
Marcos Dênis <dee.alves2@gmail.com>
2020-06-29 12:35:51 -0300

    Listando todas categorias com DTO

--- src/main/java/com/denisalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/denisalves/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.denisalves.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.denisalves.cursomc.domain.Categoria;
+import com.denisalves.cursomc.dto.CategoriaDTO;
 import com.denisalves.cursomc.services.CategoriaService;
 
 @RestController
@@ -20,6 +23,13 @@ public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaService service;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
@@ -47,4 +57,5 @@ public class CategoriaResource {
 		service.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
+	
 }
--- src/main/java/com/denisalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/denisalves/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.denisalves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,10 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repository; 
 
+	public List<Categoria> findAll() {
+		return repository.findAll();
+	}
+	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -42,4 +47,5 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que esteja veiculada a produtos");
 		}
 	}
+	
 }
