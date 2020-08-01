40fd35416f67a39ec4b8da804c49791da6874ebc
Marcos Dênis <dee.alves2@gmail.com>
2020-08-01 20:22:10 -0300

    MockEmailService com Logger. Padrões Strategy e Template method

---
+++ src/main/java/com/denisalves/cursomc/services/EmailService.java
@@ -0,0 +1,12 @@
+package com.denisalves.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.denisalves.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	void sendOrderConfirmationEmail(Pedido obj);
+		
+	void sendEmailConfirmationEmail(SimpleMailMessage obj);
+}
--- src/main/java/com/denisalves/cursomc/services/PedidoService.java
+++ src/main/java/com/denisalves/cursomc/services/PedidoService.java
@@ -37,6 +37,9 @@ public class PedidoService {
 	
 	@Autowired
 	private ClienteService clienteService;
+	
+	@Autowired
+	private EmailService emailService;
 
 	public Pedido findById(Integer id) {
 		Optional<Pedido> obj = repository.findById(id);
@@ -63,7 +66,7 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		System.out.println(obj);
+		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
 }
