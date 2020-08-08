aa4d84ecaf7b69779705a21ebcbc22dc08970e87
Marcos Dênis <dee.alves2@gmail.com>
2020-08-08 00:12:01 -0300

    Esqueci a senha

--- src/main/java/com/denisalves/cursomc/config/SecurityConfig.java
+++ src/main/java/com/denisalves/cursomc/config/SecurityConfig.java
@@ -43,11 +43,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS_GET = {
  			"/produtos/**",
-			"/categorias/**",
+			"/categorias/**"
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
- 			"/produtos/**"
+ 			"/clientes/**",
+			"/auth/forgot/**"
 	};
 
 	@Override
@@ -59,10 +60,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_POST).permitAll()
-		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.anyRequest().authenticated();
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
